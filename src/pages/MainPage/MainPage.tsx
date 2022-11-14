e943604f73221294407cb19785884ff81b0dac91
Metin Ahmad <metinmayi@gmail.com>
2022-11-14 20:36:13 +0100

    Included Header in the page

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,10 @@
+import { Header } from "./Header/Header";
 import { StyledContainer } from "./StyledContainer";
 
 export const MainPage: React.FC = () => {
-  return <StyledContainer className="d-flex" />;
+  return (
+    <StyledContainer>
+      <Header />
+    </StyledContainer>
+  );
 };
