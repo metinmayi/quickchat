1c317db3d4717983557fd37158c8429bd3046a11
Metin Ahmad <metinmayi@gmail.com>
2022-11-15 13:04:58 +0100

    Added max-width

--- src/pages/MainPage/NavbarComponent/StyledNavbar.ts
+++ src/pages/MainPage/NavbarComponent/StyledNavbar.ts
@@ -3,8 +3,10 @@ import styled from "styled-components";
 
 export const StyledNavbar = styled(Navbar)`
   padding: 1.5em;
-  border-bottom: 2px solid black;
+  border: 2px solid black;
   border-radius: 10px;
   transition: top 0.5s;
+  max-width: 500px;
+  margin: auto;
   top: ${(props) => (props.$isCollapsed ? "-14%" : "0")};
 `;
--- src/pages/MainPage/SendMessageComponent/SendMessageComponent.tsx
+++ src/pages/MainPage/SendMessageComponent/SendMessageComponent.tsx
@@ -1,14 +1,25 @@
+import styled from "styled-components";
 import { InputWrapper } from "./InputWrapper";
 import { StyledInput } from "./StyledInput";
 import { StyledSend } from "./StyledSend";
 
 export const SendMessageComponent: React.FC = () => {
   return (
-    <div className="fixed-bottom d-flex justify-content-center mb-3">
+    <StyledWrapper className="fixed-bottom d-flex justify-content-center mb-3">
       <InputWrapper>
         <StyledInput type="text" />
         <StyledSend size={25}></StyledSend>
       </InputWrapper>
-    </div>
+    </StyledWrapper>
   );
 };
+
+export const StyledWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  margin-bottom: 2vh;
+  margin: auto;
+  max-width: 500px;
+`;
