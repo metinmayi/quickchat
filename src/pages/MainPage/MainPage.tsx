8e816754b61d9a4615ce10b05c95be3c9ebe1782
Metin Ahmad <metinmayi@gmail.com>
2022-11-17 00:06:28 +0100

    MainPage now passes down props

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -6,13 +6,20 @@ import { NavbarComponent } from "./NavbarComponent/NavbarComponent";
 import { SendMessageComponent } from "./SendMessageComponent/SendMessageComponent";
 
 export const MainPage: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [chatRoom, setChatRoom] = useState("");
-  const [chatRooms, setChatRooms] = useState("");
+  const [username, setUsername] = useState("John/Jane Doe");
+  const [chatRoom, setChatRoom] = useState("General Chat");
+  const [chatRooms, setChatRooms] = useState(["General Chat"]);
 
   return (
     <StyledContainer>
-      <NavbarComponent />
+      <NavbarComponent
+        chatRooms={chatRooms}
+        setChatRooms={setChatRooms}
+        chatRoom={chatRoom}
+        setChatRoom={setChatRoom}
+        username={username}
+        setUsername={setUsername}
+      />
       <ChatAreaComponent />
       <SendMessageComponent />
     </StyledContainer>
