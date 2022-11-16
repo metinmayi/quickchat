beab2d03765b16b753825f58a9499abc665c83bd
Metin Ahmad <metinmayi@gmail.com>
2022-11-17 00:06:38 +0100

    Navbar usage of props

--- src/pages/MainPage/NavbarComponent/NavbarComponent.tsx
+++ src/pages/MainPage/NavbarComponent/NavbarComponent.tsx
@@ -1,15 +1,35 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { ChatRoomsMenu } from "./ChatRoomsMenu/ChatRoomsMenu";
 import { LogoAndHamburger } from "./LogoAndHamburger/LogoAndHamburger";
 import { StyledNavbar } from "./StyledNavbar";
 import { UsernameInput } from "./UsernameInput/UsernameInput";
 
-export const NavbarComponent: React.FC = () => {
+interface Props {
+  chatRooms: string[];
+  chatRoom: string;
+  username: string;
+  setChatRooms: React.Dispatch<React.SetStateAction<string[]>>;
+  setChatRoom: React.Dispatch<React.SetStateAction<string>>;
+  setUsername: React.Dispatch<React.SetStateAction<string>>;
+}
+export const NavbarComponent: React.FC<Props> = ({
+  chatRooms,
+  setChatRooms,
+  chatRoom,
+  setChatRoom,
+  username,
+  setUsername,
+}) => {
   const [isCollapsed, setIsCollapsed] = useState(true);
   return (
     <StyledNavbar fixed="top" className="d-block" $isCollapsed={isCollapsed}>
-      <ChatRoomsMenu />
-      <UsernameInput />
+      <ChatRoomsMenu
+        chatRooms={chatRooms}
+        setChatRooms={setChatRooms}
+        chatRoom={chatRoom}
+        setChatRoom={setChatRoom}
+      />
+      <UsernameInput username={username} setUsername={setUsername} />
       <LogoAndHamburger setIsCollapsed={setIsCollapsed} />
     </StyledNavbar>
   );
