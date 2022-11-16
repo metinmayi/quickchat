dedf8918a258a87365b6d141864cb0fa3226a7ab
Metin Ahmad <metinmayi@gmail.com>
2022-11-17 00:07:00 +0100

    Can now set username

--- src/pages/MainPage/NavbarComponent/UsernameInput/UsernameInput.tsx
+++ src/pages/MainPage/NavbarComponent/UsernameInput/UsernameInput.tsx
@@ -1,11 +1,18 @@
+import React from "react";
 import { Col, Row } from "react-bootstrap";
 
-export const UsernameInput: React.FC = () => {
+interface Props {
+  username: string;
+  setUsername: React.Dispatch<React.SetStateAction<string>>;
+}
+export const UsernameInput: React.FC<Props> = ({ username, setUsername }) => {
   return (
     <Row>
       <Col>
         <h6 className="m-0">Username:</h6>
-        <input defaultValue="Metin"></input>
+        <input
+          value={username}
+          onChange={(e) => setUsername(e.currentTarget.value)}></input>
       </Col>
     </Row>
   );
