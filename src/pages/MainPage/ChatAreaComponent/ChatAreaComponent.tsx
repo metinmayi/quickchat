a1f4e700c196af58b544aaa9b2624da1ae9a3e4e
Metin Ahmad <metinmayi@gmail.com>
2022-11-18 01:31:25 +0100

    Image is on right side if sender is user

--- src/pages/MainPage/ChatAreaComponent/ChatAreaComponent.tsx
+++ src/pages/MainPage/ChatAreaComponent/ChatAreaComponent.tsx
@@ -18,17 +18,35 @@ export const ChatAreaComponent: React.FC<Props> = ({
   const senderIsMe = username === sender;
   return (
     <Row className="d-flex justify-content-center">
-      <Col xs={8}>
-        <Row>
-          <StyledParagraph>{senderIsMe ? "Me" : sender}</StyledParagraph>
+      {senderIsMe ? (
+        <Row className="d-flex justify-content-center">
+          <Col xs={8}>
+            <Row>
+              <StyledParagraph>Me</StyledParagraph>
+            </Row>
+            <Row>
+              <StyledChatBubble>{message}</StyledChatBubble>
+            </Row>
+          </Col>
+          <Col xs={2}>
+            <StyledImage src={avatar} />
+          </Col>
         </Row>
-        <Row>
-          <StyledChatBubble>{message}</StyledChatBubble>
-        </Row>
-      </Col>
-      <Col xs={2}>
-        <StyledImage src={avatar} />
-      </Col>
+      ) : (
+        <>
+          <Col xs={2}>
+            <StyledImage src={avatar} />
+          </Col>
+          <Col xs={8}>
+            <Row>
+              <StyledParagraph>{sender}</StyledParagraph>
+            </Row>
+            <Row>
+              <StyledChatBubble>{message}</StyledChatBubble>
+            </Row>
+          </Col>
+        </>
+      )}
     </Row>
   );
 };
