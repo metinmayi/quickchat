d1dcb07371c45f60e2675433222a9b429ba10ac5
Metin Ahmad <metinmayi@gmail.com>
2022-11-18 01:11:38 +0100

    Can now write and send messages

--- src/pages/MainPage/SendMessageComponent/SendMessageComponent.tsx
+++ src/pages/MainPage/SendMessageComponent/SendMessageComponent.tsx
@@ -1,14 +1,31 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { socket } from "../../../App";
 import { InputWrapper } from "./InputWrapper";
 import { StyledInput } from "./StyledInput";
 import { StyledSend } from "./StyledSend";
 
-export const SendMessageComponent: React.FC = () => {
+interface Props {
+  username: string;
+  chatRoom: string;
+}
+export const SendMessageComponent: React.FC<Props> = ({
+  username,
+  chatRoom,
+}) => {
+  const [message, setMessage] = useState("");
+  function sendMessage() {
+    socket.emit("sendMessage", { message, username, chatRoom });
+  }
   return (
     <StyledWrapper className="fixed-bottom d-flex justify-content-center mb-3">
       <InputWrapper>
-        <StyledInput type="text" />
-        <StyledSend size={25}></StyledSend>
+        <StyledInput
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.currentTarget.value)}
+        />
+        <StyledSend size={25} onClick={sendMessage}></StyledSend>
       </InputWrapper>
     </StyledWrapper>
   );
