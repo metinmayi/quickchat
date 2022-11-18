f8944e24597a8bc97dcbbd3376c24f9e8bced76a
Metin Ahmad <metinmayi@gmail.com>
2022-11-18 01:10:58 +0100

    Added ability to switch between rooms

--- server/index.js
+++ server/index.js
@@ -12,14 +12,28 @@ const io = new Server(server, {
   },
 });
 
-app.get("/", (req, res) => {
-  res.send("<h1>Hello world</h1>");
-});
-
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  socket.on("sendMessage", sendMessage);
+  socket.on("joinRoom", joinRoom);
 });
 
 server.listen(4000, () => {
   console.log("listening on *:3000");
 });
+
+function sendMessage(data) {
+  this.broadcast.to(data.chatRoom).emit("broadcastMessage", {
+    message: data.message,
+    username: data.username,
+  });
+}
+
+function joinRoom(data) {
+  const userRooms = io.sockets.adapter.sids.get(this.id);
+
+  for (const room of userRooms) {
+    this.leave(room);
+  }
+
+  this.join(data.chatRoom);
+}
