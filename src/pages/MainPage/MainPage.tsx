a1e3990ad302820c35eeb776f8c2d09863f79a9b
Metin Ahmad <metinmayi@gmail.com>
2022-11-18 01:08:25 +0100

    MainPage now retrieves and renders chat messages

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
 import styled from "styled-components";
+import { socket } from "../../App";
 import { ChatAreaComponent } from "./ChatAreaComponent/ChatAreaComponent";
 import { NavbarComponent } from "./NavbarComponent/NavbarComponent";
 import { SendMessageComponent } from "./SendMessageComponent/SendMessageComponent";
 
+interface Message {
+  username: string;
+  message: string;
+}
 export const MainPage: React.FC = () => {
   const [username, setUsername] = useState("John/Jane Doe");
   const [chatRoom, setChatRoom] = useState("General Chat");
   const [chatRooms, setChatRooms] = useState(["General Chat"]);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    socket.on("broadcastMessage", (socket) => {
+      const newMessage = {
+        username: socket.username as string,
+        message: socket.message as string,
+      };
+      setMessages([...messages, newMessage]);
+    });
+  });
 
   return (
     <StyledContainer>
@@ -20,12 +36,21 @@ export const MainPage: React.FC = () => {
         username={username}
         setUsername={setUsername}
       />
-      <ChatAreaComponent />
-      <SendMessageComponent />
+      {messages.map((message) => (
+        <ChatAreaComponent
+          username={message.username}
+          message={message.message}
+          key={message.message}
+        />
+      ))}
+      <SendMessageComponent username={username} chatRoom={chatRoom} />
     </StyledContainer>
   );
 };
 
 export const StyledContainer = styled(Container)`
   max-width: 750px !important;
+  & .row:nth-child(3) {
+    margin-top: 5vh !important;
+  }
 `;
